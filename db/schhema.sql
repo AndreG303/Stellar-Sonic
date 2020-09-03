033c9d2737d230ea275bc8fdbf0428d5db263bf1
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-09-02 20:06:38 -0400

    added db

---
+++ db/schhema.sql
@@ -0,0 +1,19 @@
+-- DROP DATABASE IF EXISTS music_db;
+-- CREATE database music_db;
+
+USE music_db;
+
+CREATE TABLE top5000 (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  song VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+SELECT * FROM top5000;

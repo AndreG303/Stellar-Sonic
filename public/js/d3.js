c8d8d5ed9fe5e669eba973e5beb681e7dde760d5
Andrea Gamez <andreamgamez@gmail.com>
2020-09-03 18:12:33 -0400

    created labels with D3

--- public/js/d3.js
+++ public/js/d3.js
@@ -17,9 +17,9 @@
 // d3.select('body')
 //     .selectAll('p')
 //     .data(dataset)
-    // .enter()//take data elements one by one 
-    // .append('p')//appends paragraph for each data element 
-    // .text('D3 is awesome!');
+// .enter()//take data elements one by one 
+// .append('p')//appends paragraph for each data element 
+// .text('D3 is awesome!');
 // .text(function(d){return d; });
 
 var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
@@ -29,23 +29,35 @@ var svgWidth = 500, svgHeight = 300, barPadding = 5;
 var barWidth = (svgWidth / dataset.length);
 
 var svg = d3.select('svg')
-.attr("width", svgWidth)
-.attr("height", svgHeight);
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
 var barchart = svg.selectAll("rect")
-.data(dataset)
-.enter()
-.append("rect")
-.attr("y", function(d){
-    return svgHeight - d 
-})
-.attr("height", function(d){
-    return d;
-})
-.attr("width", barWidth - barPadding)
-.attr("transform", function (d, i){
-    var translate = [barWidth * i, 0];
-    return "translate("+ translate +")";
-});
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function (d) {
+        return svgHeight - d
+    })
+    .attr("height", function (d) {
+        return d;
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", function (d, i) {
+        var translate = [barWidth * i, 0];
+        return "translate(" + translate + ")";
+    });
 
-
+var text = svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function (d) {
+        return d;
+    })
+    .attr("y", function (d, i) {
+        return svgHeight - d - 2;
+    })
+    .attr("x", function (d, i) {
+        return barWidth * i;
+    })

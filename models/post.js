96abb8a4a7712048e707cc6ab6629a1ae52a29a2
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-09-05 22:09:40 -0400

    adding some api rputes , two models some code the the members html file, addition to members js

---
+++ models/post.js
@@ -0,0 +1,27 @@
+// Add a flag for the text attribute to prevent this field from being null
+
+// Add a validation for the text attribute to make sure it's at least one character,
+// but no more than 140 characters
+
+// Add a flag for complete so that it's false by default if not given a value
+
+module.exports = function(sequelize, DataTypes) {
+  var Post = sequelize.define("Post", {
+    author: { 
+      type: DataTypes.STRING,
+      allowNull:false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+    body: { 
+      type: DataTypes.STRING,
+      allowNull:false,
+      validate: {
+        len: [1, 140]
+      }
+    }
+    // complete: {type: DataTypes.BOOLEAN, defaultValue: false }
+  });
+  return Post;
+};

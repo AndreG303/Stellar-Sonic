bdfb1f04028b27513a502a52b33e8fb767827f26
Andrew Dalba <andrewdalba@gmail.com>
2020-09-09 19:27:00 -0400

    change login method

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
 const db = require("../models");
+const {Op} = db.Sequelize;
 
 // Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
 passport.use(
@@ -14,7 +15,10 @@ passport.use(
       // When a user tries to sign in this code runs
       db.User.findOne({
         where: {
-          email: email
+          [Op.or]: [
+            {email: email}
+            , {username: email}
+          ]
         }
       }).then(dbUser => {
         // If there's no user with the given email
@@ -35,6 +39,38 @@ passport.use(
     }
   )
 );
+
+// passport.use(
+//   new LocalStrategy(
+//     // Our user will sign in using an username, rather than a "username"
+//     {
+//       usernameField: "username"
+//     },
+//     (username, password, done) => {
+//       // When a user tries to sign in this code runs
+//       db.User.findOne({
+//         where: {
+//           username: username
+//         }
+//       }).then(dbUser => {
+//         // If there's no user with the given username
+//         if (!dbUser) {
+//           return done(null, false, {
+//             message: "Incorrect username."
+//           });
+//         }
+//         // If there is a user with the given username, but the password the user gives us is incorrect
+//         else if (!dbUser.validPassword(password)) {
+//           return done(null, false, {
+//             message: "Incorrect password."
+//           });
+//         }
+//         // If none of the above, return the user
+//         return done(null, dbUser);
+//       });
+//     }
+//   )
+// );
 
 // In order to help keep authentication state across HTTP requests,
 // Sequelize needs to serialize and deserialize the user

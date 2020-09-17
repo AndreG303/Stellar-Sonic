809925cd75733fb472f8c7979ad567706a32f04a
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-09-17 03:25:25 -0400

    added picture upload posibility, scrolling console.log embed

--- models/user.js
+++ models/user.js
@@ -21,6 +21,11 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+     // The password cannot be null
+     profilePicture: {
+      type: DataTypes.STRING,
+      allowNull: true
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database

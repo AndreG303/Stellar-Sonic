458b68c5bb98d72e872573836f48a464251d4324
Andrew Dalba <andrewdalba@gmail.com>
2020-09-18 18:46:50 -0400

    cleanup html-routes js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,14 +1,10 @@
-
 const db = require("../models");
 const passport = require("../config/passport");
 const { sequelize } = require("../models");
-
 const { QueryTypes } = db.Sequelize;// Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
-
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
-
 module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
@@ -17,7 +13,6 @@ module.exports = function(app) {
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
-
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
@@ -25,13 +20,9 @@ module.exports = function(app) {
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
-
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
-  
-   
-   
 };

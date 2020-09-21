7c2ca68818e3b6381dc78fd4fedf475857776dfc
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-09-21 08:45:47 -0400

    added a button to close the play- box

--- public/js/members.js
+++ public/js/members.js
@@ -344,11 +344,12 @@ var row3c = $('<div class="card-body" id="search-display1">');
 
 var row3e = $('<p><iframe class="jvideo" width="580" height="360" src=' + fixedlink1+ '>g</iframe> </p>');
 
-// var row3i = $('<button type="button" class="btn btn-shazamAdd" onkeyup="document.location.reload(true)">Add to playlist</button>');
+var row3k = $('<button type="button" class="btn btn-close-play-box" onkeyup="document.location.reload(true)">close</button>');
 var row3g = $('</div>');
 row3.append(row3a);
 row3.append(row3b);
 row3.append(row3c);
+row3.append(row3e);
 row3.append(row3e);
 
 row3.append(row3g);
